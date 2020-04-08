89c476711068450625f1f46cf85f39a63b5a9a95
Andrej Luzginov <andrej@Air-Andrej.lan>
2020-04-08 16:02:49 +0300

    upload my first release

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.sl').slick({
+      prevArrow: '.arrow_prev',
+      nextArrow: '.arrow_next',
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+
+    $('.testimon').slick({
+      prevArrow: '.testimon__prev',
+      nextArrow: '.testimon__next',
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+  });
